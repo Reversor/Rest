7e270207fd8cbfb605a09095e595513c507b45e2
Rost Zhiharev <revers914@gmail.com>
2018-03-19 13:22:20 +0400

    f*ck submodules

--- roach/src/main/java/JAXActivator.java
+++ roach/src/main/java/JAXActivator.java
@@ -1,5 +1,4 @@
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-import io.swagger.v3.oas.integration.SwaggerConfiguration;
 import java.util.HashSet;
 import java.util.Set;
 import javax.ws.rs.ApplicationPath;
@@ -10,19 +9,11 @@ import rest.RoachEndpoint;
 @ApplicationPath("/")
 public class JAXActivator extends Application {
 
-    public JAXActivator() {
-        init();
-    }
-
-    private void init() {
-        SwaggerConfiguration configuration = new SwaggerConfiguration();
-    }
-
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> resources = new HashSet<>();
-        resources.add(RoachEndpoint.class);
         resources.add(OpenApiResource.class);
+        resources.add(RoachEndpoint.class);
         return resources;
     }
 
