c0909daef26650d5988f130b22075f956793782e
Rost Zhiharev <revers914@gmail.com>
2018-03-22 21:38:35 +0400

    fix initialization order

--- roach/src/main/java/services/NodeManager.java
+++ roach/src/main/java/services/NodeManager.java
@@ -3,13 +3,16 @@ package services;
 import dao.NodeDao;
 import entities.Node;
 import java.util.Set;
+import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
+import javax.ejb.Startup;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.jboss.logging.Logger;
 
+@Startup
 @Singleton
 public class NodeManager {
 
@@ -17,20 +20,18 @@ public class NodeManager {
     NodeDao nodeDao;
     Client client;
     private Logger logger = Logger.getLogger(this.getClass());
+    Set<Node> nodes;
 
-    public NodeManager() {
-        init();
-    }
-
-    private void init() {
+    @PostConstruct
+    private void init(){
         client = ClientBuilder.newClient();
+        nodes = nodeDao.getAll();
     }
 
     @Schedule(second = "*/20")
     public void checkNodes() {
-        Set<Node> nodes = nodeDao.getAll();
         logger.info("Check nodes");
         logger.info(nodes);
-        // TODO Http-client neededl
+        // TODO Http-client needed
     }
 }
